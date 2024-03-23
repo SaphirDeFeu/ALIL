07ecfffeb4b3066a259430a67db10e70e0e679c8
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2024-03-23 15:39:45 +0100

    Primitive CLI
    - Help and Version commands available

--- __index__.ts
+++ __index__.ts
@@ -1,3 +1,16 @@
 import { main } from "./src/main.ts";
 
-Deno.exit(main());
+export interface SemanticVersion {
+  Major: number;
+  minor: number;
+  patch: number;
+}
+
+const version: string[] = Deno.readTextFileSync("./version.txt").split(".");
+const sem_ver: SemanticVersion = {
+  Major: parseInt(version[0]),
+  minor: parseInt(version[1]),
+  patch: parseInt(version[2]),
+};
+
+Deno.exit(main(sem_ver));
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,31 @@
-export function main(): number {
-  console.log("Hello, World!");
+import { parseArgs } from "@std/cli/parse_args.ts";
+import { SemanticVersion } from "../__index__.ts";
+
+export function main(version: SemanticVersion): number {
+  const args = parseArgs(Deno.args);
+
+  if (args.h || args.help) {
+    displayHelp();
+    return 0;
+  }
+
+  if (args.V || args.version) {
+    console.log(
+      `ALIL ${version.Major}.${version.minor}.${version.patch} by TSMStudios`
+    );
+    return 0;
+  }
+
+  if (args._.length == 0) {
+    displayHelp();
+    return 0;
+  }
+
   return 0;
+}
+
+function displayHelp() {
+  console.log(
+    `ALIL - Assembly-Like Interpreted Language\n\nUsage: alil [options] <file>\n\nOptions:\n  -h, --help     Print help message\n  -V, --version  Print version info\n  -v, --verbose  Allow printing of ALIL log messages\n  -c, --config   Specify a config file for the interpreter to use\n`
+  );
 }
