1820440f1d8d0246bf6bcb6b87991e4a0343db01
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2024-03-23 15:51:25 +0100

    Version control
    - If running an older version of ALIL, you are told that when running `alil -V`

--- __index__.ts
+++ __index__.ts
@@ -1,16 +1,40 @@
 import { main } from "./src/main.ts";
 
-export interface SemanticVersion {
+export class SemanticVersion {
   Major: number;
   minor: number;
   patch: number;
+
+  constructor(version: string) {
+    const split = version.split(".");
+    this.Major = parseInt(split[0]);
+    this.minor = parseInt(split[1]);
+    this.patch = parseInt(split[2]);
+  }
+
+  toString(): string {
+    return `${this.Major}.${this.minor}.${this.patch}`;
+  }
+
+  // If `other_version` is the latest of the two versions, return true. Otherwise, false
+  lessThan(other_version: SemanticVersion): boolean {
+    if (this.Major < other_version.Major) {
+      return true;
+    }
+
+    if (this.minor < other_version.minor) {
+      return true;
+    }
+
+    if (this.patch < other_version.patch) {
+      return true;
+    }
+
+    return false;
+  }
 }
 
-const version: string[] = Deno.readTextFileSync("./version.txt").split(".");
-const sem_ver: SemanticVersion = {
-  Major: parseInt(version[0]),
-  minor: parseInt(version[1]),
-  patch: parseInt(version[2]),
-};
+const version: string = Deno.readTextFileSync("./version.txt");
+const sem_ver: SemanticVersion = new SemanticVersion(version);
 
-Deno.exit(main(sem_ver));
+Deno.exit(await main(sem_ver));
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { parseArgs } from "@std/cli/parse_args.ts";
 import { SemanticVersion } from "../__index__.ts";
 
-export function main(version: SemanticVersion): number {
+export async function main(version: SemanticVersion): Promise<number> {
   const args = parseArgs(Deno.args);
 
   if (args.h || args.help) {
@@ -10,9 +10,26 @@ export function main(version: SemanticVersion): number {
   }
 
   if (args.V || args.version) {
-    console.log(
-      `ALIL ${version.Major}.${version.minor}.${version.patch} by TSMStudios`
+    console.log(`ALIL ${version.toString()} by TSMStudios`);
+    console.log(`   \x1b[103mCLI\x1b[0m Fetching versions`);
+    const latest_version: string = await (
+      await fetch(
+        "https://raw.githubusercontent.com/SaphirDeFeu/ALIL/master/version.txt"
+      )
+    ).text();
+    const latest_sem_version: SemanticVersion = new SemanticVersion(
+      latest_version
     );
+
+    if (version.lessThan(latest_sem_version)) {
+      console.log(
+        `New version of ALIL available (${latest_sem_version.toString()}) : https://github.com/SaphirDeFeu/ALIL/releases/tag/v${latest_sem_version.toString()}`
+      );
+    } else {
+      console.log(
+        `You are running the latest version of ALIL (${version.toString()})`
+      );
+    }
     return 0;
   }
 
